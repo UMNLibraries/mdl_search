a549e55ac065403ced43f1891e8b46e21a950418
chadfennell <libsys@gmail.com>
2018-03-23 12:46:10 -0500

    dev is done locally; clarify that it happens in vagrant

---
+++ config/deploy/vagrant.rb
@@ -0,0 +1,17 @@
+set :ssh_options, keys:  ['/home/fenne035/dev/vms/mdl/ansible-playbook-blacklight/.vagrant/machines/blacklight/virtualbox/private_key']
+role :web, "swadm@127.0.0.1:2222"
+set :ssh_options, {:forward_agent => true, :keys =>  ['/home/fenne035/dev/vms/mdl/ansible-playbook-blacklight/.vagrant/machines/blacklight/virtualbox/private_key']}
+
+set :deploy_to, '/swadm/var/www/blacklight'
+set :use_sudo, false
+
+set :linked_dirs, %w(thumbnails log public/assets tmp/pids tmp/sockets)
+
+set :rails_env, "production"
+
+set :bundle_flags, '--deployment'
+
+set :sidekiq_concurrency, 1
+set :sidekiq_processes, 2
+
+set :branch, 'feature/ansible-v-2'
