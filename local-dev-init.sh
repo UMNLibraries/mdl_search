b1dc534c5ac16fab259aa344d8813cf60d803a6c
Chad Fennell <libsys@gmail.com>
2019-05-14 17:08:08 -0500

    adding a second mysql container didn't work too well in practice; just create a test db in the db container

--- local-dev-init.sh
+++ local-dev-init.sh
@@ -7,5 +7,8 @@ git clone https://github.com/UMNLibraries/mdl-solr-core.git;
 # Build, start the app
 mkdir -p public/assets/thumbnails;
 docker-compose build;
-docker-compose run web rake db:migrate
 docker-compose up;
+docker-compose down;
+docker-compose run web rake db:migrate
+docker-compose exec db mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS mdl_test; GRANT ALL PRIVILEGES ON mdl_test.* TO 'mdl'@'%'"
+docker-compose exec -e "RAILS_ENV=test" web rake db:migrate
