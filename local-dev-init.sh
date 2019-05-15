5b432f6942fe3b1e583a2f431b308e70b38e2346
Chad Fennell <libsys@gmail.com>
2019-05-15 11:33:05 -0500

    smooths-out the setup order

--- local-dev-init.sh
+++ local-dev-init.sh
@@ -7,10 +7,9 @@ git clone https://github.com/UMNLibraries/mdl-solr-core.git;
 # Build, start the app
 mkdir -p public/assets/thumbnails;
 docker-compose build;
-docker-compose up;
-docker-compose down;
 docker-compose run web rake db:migrate
-docker-compose exec db mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS mdl_test; GRANT ALL PRIVILEGES ON mdl_test.* TO 'mdl'@'%'"
-docker-compose exec -e "RAILS_ENV=test" web rake db:migrate
+docker-compose run db mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS mdl_test; GRANT ALL PRIVILEGES ON mdl_test.* TO 'mdl'@'%'"
+docker-compose run -e "RAILS_ENV=test" web rake db:migrate
 docker-compose run web yarn install
 sudo chown -R $(whoami) node_modules
+docker-compose up;
