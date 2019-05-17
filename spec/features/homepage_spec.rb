089d5f046bc47b2a72f938cb554a63216ec6ca35
Chad Fennell <libsys@gmail.com>
2019-05-17 13:30:28 -0500

    new test index is only 8 records

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -4,7 +4,7 @@ describe 'Some home page sanity checks' do
     it 'pins records to the home page map and allows users to navigate to them' do
       Capybara.current_driver = :selenium
       visit '/'
-      expect(page).to have_content('Browse all 52,562 items from 189 historical societies, libraries, archives, and cultural organizations across Minnesota')
+      expect(page).to have_content('Browse all 8 items from 189 historical societies, libraries, archives, and cultural organizations across Minnesota')
       find(:xpath, '//*[@id="search"]').click
       expect(page).to have_content('A. Aberle residence, 214 South Peck Street, Fergus Falls, Minnesota')
     end
