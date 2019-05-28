486b4eb910bf1381eb1adba7577bc56de5456237
Chad Fennell <libsys@gmail.com>
2019-05-28 14:48:14 -0500

    allows dev caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,21 +15,14 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
-    }
+    config.cache_store = :redis_store, ENV['REDIS_CACHE_URL']
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
