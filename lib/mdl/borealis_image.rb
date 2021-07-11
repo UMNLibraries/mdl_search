35150acfac093aa671dd3f5244e2ca0b1e0e4630
Dan Olson <olson_dan@yahoo.com>
2021-07-11 09:21:22 -0400

    Remove the 1920x1920 image download link

--- lib/mdl/borealis_image.rb
+++ lib/mdl/borealis_image.rb
@@ -12,8 +12,7 @@ module MDL
     def downloads
       [
         { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/#{collection}/#{id}/full/150,150/0/default.jpg", label: '(150 x 150)' },
-        { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/#{collection}/#{id}/full/800,800/0/default.jpg", label: '(800 x 800)' },
-        { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/#{collection}/#{id}/full/1920,1920/0/default.jpg", label: '(1920 x 1920)' }
+        { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/#{collection}/#{id}/full/800,800/0/default.jpg", label: '(800 x 800)' }
       ]
     end
 
